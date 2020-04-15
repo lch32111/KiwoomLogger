450dca6c2b1f6d082a2ebef830460d28c22c5f6c
Chanhaeng Lee <lch32111@naver.com>
2020-04-15 18:15:50 +0900

    init for project

---
+++ include/KLVersion.h
@@ -0,0 +1,9 @@
+#pragma once
+#ifndef __KL_VERSION_H__
+#define __KL_VERSION_H__
+
+#define KL_VERSION_MAJOR 1
+#define KL_VERSION_MINOR 0
+#define KL_VERSION_PATCH 0
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "KLVersion.h"
+
+int main(void)
+{
+    std::cout << "Hello World" << '\n';
+    std::cout << KL_VERSION_MAJOR << ' ' << KL_VERSION_MINOR << '\n';
+}
